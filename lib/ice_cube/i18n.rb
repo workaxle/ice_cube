c0a1c7956e0d40dd60e854271eb2438bb3463cae
vadshalamov <vadshalamov@gmail.com>
2023-10-18 18:10:31 -0400

    fix: adapt i18n to ruby 3.x changes

--- lib/ice_cube/i18n.rb
+++ lib/ice_cube/i18n.rb
@@ -2,15 +2,14 @@ require 'ice_cube/null_i18n'
 
 module IceCube
   module I18n
-
     LOCALES_PATH = File.expand_path(File.join('..', '..', '..', 'config', 'locales'), __FILE__)
 
-    def self.t(*args)
-      backend.t(*args)
+    def self.t(*args, **kwargs)
+      backend.t(*args, **kwargs)
     end
 
-    def self.l(*args)
-      backend.l(*args)
+    def self.l(*args, **kwargs)
+      backend.l(*args, **kwargs)
     end
 
     def self.backend
